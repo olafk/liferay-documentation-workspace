d3678f731a12ad21d179676158eb8cbfeac29dba
Olaf Kock <github@olafkock.de>
2020-06-10 13:17:34 +0200

    dummy implementation for a commerce accellerator site

---
+++ modules/documentation-resolver-commerce/src/main/java/de/olafkock/liferay/documentation/resolver/commerce/DocumentationEntryImpl.java
@@ -0,0 +1,51 @@
+package de.olafkock.liferay.documentation.resolver.commerce;
+
+import de.olafkock.liferay.documentation.api.DocumentationEntry;
+
+public class DocumentationEntryImpl implements DocumentationEntry {
+
+	public DocumentationEntryImpl(String documentationUrl, String audioUrl, String scriptUrl) {
+		this.documentationUrl = documentationUrl;
+		this.audioUrl = audioUrl;
+		this.scriptUrl = scriptUrl;
+	}
+	
+	@Override
+	public String getDocumentationUrl() {
+		return documentationUrl;
+	}
+
+	@Override
+	public String getAudioURL() {
+		return audioUrl;
+	}
+
+	@Override
+	public String getScriptURL() {
+		return scriptUrl;
+	}
+
+	@Override
+	public boolean isDocumented() {
+		return hasContent(documentationUrl);
+	}
+
+	@Override
+	public boolean isAudio() {
+		return hasContent(audioUrl);
+	}
+
+	@Override
+	public boolean isScripted() {
+		return hasContent(scriptUrl);
+	}
+
+	private boolean hasContent(String string) {
+		return string != null && ! string.isEmpty();
+	}
+	
+	private String documentationUrl;
+	private String audioUrl;
+	private String scriptUrl;
+
+}
