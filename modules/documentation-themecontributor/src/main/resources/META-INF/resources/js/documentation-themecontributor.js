32a3a08378e26774f8f6675c86979a11b5047a2e
olaf <olaf@t19>
2020-10-14 15:34:40 +0200

    eliminate jquery

--- modules/documentation-themecontributor/src/main/resources/META-INF/resources/js/documentation-themecontributor.js
+++ modules/documentation-themecontributor/src/main/resources/META-INF/resources/js/documentation-themecontributor.js
@@ -5,47 +5,47 @@
 audioguide_initializeAndPlay = function(thePlayer, scripts) {
 
 	var audioguide = scripts[0];
-	var lastUpdate = 0;
+	var lastUpdate = -1;
 	console.log("initializing audioguide script " + audioguide["name"] + " with " + audioguide["media"]);
-	thePlayer.attr("src", audioguide["media"]);
+	thePlayer.src = audioguide["media"];
 
-	thePlayer.bind("timeupdate", function() {
+	thePlayer.ontimeupdate = function() {
 		// run max once per second to avoid firing twice.
 		// events come in multiple times per second.
 		// On slower environments, we might miss a beat - 
 		// is that an issue?
 		var player = this;
 		var currentTime = player.currentTime;
-		
+				
 		if(parseInt(lastUpdate) == parseInt(currentTime)) {
 			return;
 		}
 		lastUpdate = parseInt(currentTime);
 
 			
-		$.each(audioguide["command"], function(index, command) {
+		audioguide["command"].forEach(function(command) {
 			var cmd       = command["command"];
-			var timeStart = parseInt(command["timeStart"]);
-			var timeEnd   = parseInt(command["timeEnd"]);
+			var timeStart = command["timeStart"];
+			var timeEnd   = command["timeEnd"];
 			var payload   = command["payload"];
 			
 			if(currentTime >= timeStart) {
 				if(cmd == "scroll" && currentTime < timeStart+1) {
 					
 					console.log("firing " + cmd + " on time " + timeStart + " to element " + payload + " " + timeEnd );
-					$(payload).each(function(e){this.scrollIntoView({block: "center", inline: "nearest", behavior:"smooth"});});
+					document.querySelectorAll(payload).forEach(function(e){this.scrollIntoView();});
 					
 				} else if(cmd == "highlight"  && currentTime <= timeEnd) {
 				
 					console.log("firing " + cmd + " on time " + timeStart + " until " + timeEnd + " to " + payload);
-					$(payload).addClass("replay-highlight");
+					document.querySelectorAll(payload).forEach(function(e){e.classList.add("replay-highlight")});
 
 					command["command"] = "unhighlight";
 					
 				} else if(cmd == "click" && currentTime < timeStart+1) {
-					
-					console.log("firing " + cmd + " on time " + timeStart + " to " + payload + "(" + $(payload).count + " elements)");
-					$(payload).each(function(){this.click();});
+					var targetElements = document.querySelectorAll(payload);
+					console.log("firing " + cmd + " on time " + timeStart + " to " + payload + "(" + targetElements.count + " elements)");
+					targetElements.forEach(function(elem){elem.click();});
 					
 				} else if(cmd == "eval" && currentTime < timeStart+1) {
 					
@@ -58,91 +58,116 @@ audioguide_initializeAndPlay = function(thePlayer, scripts) {
 			if((currentTime < timeStart || currentTime > timeEnd) && cmd == "unhighlight") {
 				
 				console.log("firing " + cmd + " on time " + timeEnd + " to " + payload);
-				$(payload).removeClass("replay-highlight");
+				document.querySelectorAll(payload).forEach(function(e){e.classList.remove("replay-highlight")});
 				command["command"] = "highlight";
 			}
 		});
-	});
-	thePlayer.trigger("play");
+	};
+	thePlayer.play();
 };
 
 
 audioguide_trigger = function() {
 	var player = audioguide_findPlayer();
 	
-	if(player.prop("paused")) {
-		player.trigger("play");
+	if(player.paused) {
+		player.play();
 	} else {
-		player.trigger("pause");
+		player.pause();
 	}
 };
 
 
 audioguide_findPlayer = function() {
-	var container = $("a[data-identifier=\"audioguide\"]");
-	var player = $("audio[data-identifier=\"audioguide\"]");
+	var container = document.querySelector("a[data-identifier=\"audioguide\"]");
+	var player = document.querySelector("audio[data-identifier=\"audioguide\"]");
 	
 	// Make sure that there is any configuration - when there's no
 	// container, the ControlMenu Icon isn't there at all.
-	if(container.length == 0) {
+	if(container == null) {
 		console.log("no audioguide on this page");
 		return;
 	}
 
-	if(player.length != 0){
+	if(player != null){
 //		console.log("found an existing audioguide player");
 	} else {
-		container.wrap("<span class='audioguidelink'></span>");
-		container.before("<audio controls data-identifier=\"audioguide\" ></audio>");
+		originalHtml = container.innerHTML;
+		container.innerHTML = "<audio controls data-identifier=\"audioguide\" ></audio><span class='audioguidelink'>" + originalHtml + "</span>";
 
-		player = $("audio[data-identifier=\"audioguide\"]");
+		player = document.querySelector("audio[data-identifier=\"audioguide\"]");
 
-		var scriptUrl = container.data("audioguide-script");
-		var audioUrl = container.data("audioguide-audio");
+//  !!!!!!!!!!!!!!!!!!
+		var scriptUrl = container.dataset.audioguidescript;
+		var audioUrl = container.dataset.audioguideaudio;
 		if(scriptUrl != audioUrl) {
 			// prevent playing json, in case we have an audio guide with script
-			player.attr("src", audioUrl);
+			player.src = audioUrl;
 		}
 		
-		player.on("play", function(){
-			container.toggleClass("audioguideplaying", true );
-		});
-		player.on("pause", function(){
-			container.toggleClass("audioguideplaying", false);
-		});
+		player.onplay = function(){
+			container.classList.add("audioguideplaying");
+		};
+		player.onpause = function(){
+			container.classList.remove("audioguideplaying");
+		};
+	
+        var http_request = new XMLHttpRequest();
 
-
-		$.getJSON(scriptUrl, "", function(data) {
-			audioguide_initializeAndPlay(player, data);
-		});
+        http_request.onreadystatechange = function() {
+           if (http_request.readyState == 4  ) {
+              var data = JSON.parse(http_request.responseText);
+              audioguide_initializeAndPlay(player, data);
+           }
+        }
+		
+        http_request.open("GET", scriptUrl, true);
+        http_request.send();
 	}
 	
 	return player;
 };
 
+
 toast_trigger = function() {
 //	console.log("triggering toast");
-	var existingToast = $(".documentation-toast");
-	if(existingToast.length == 0){
+	var existingToast = document.querySelector(".documentation-toast");
+	if(existingToast == null){
 //		console.log("creating toast");
-		var container = $("a[data-identifier=\"toast\"]");
-		if(container.length == 0) {
+		var container = document.querySelector("a[data-identifier=\"toast\"]");
+		if(container == null) {
 			console.log("no toast on this page");
 			return;
 		}
-		var toastUrl = container.data("documentation-toast");
+		var toastUrl = container.dataset.documentationtoast;
 		var toastDiv = "<div class='documentation-toast toast-hidden'>" 
 					 + "<div><a href='javascript:toast_trigger()'>" + Liferay.Language.get("close")+ "</a></div>" 
 					 + "<iframe src='" + toastUrl + "'></iframe>"
 					 + "</div>"
-		$("#main-content").append(toastDiv);
+		document.getElementById("main-content").insertAdjacentHTML('beforeend',toastDiv);
 		setTimeout(function(){
-			$(".documentation-toast").toggleClass("toast-hidden", false);
+			document.querySelector(".documentation-toast").classList.remove("toast-hidden");
 		}, 50);
 		
 	} else {
 //		console.log("toggling existing toast visibility");
-		existingToast.toggleClass("toast-hidden", ! existingToast.hasClass("toast-hidden"));
+		existingToast.classList.toggle("toast-hidden");
 	}
 	
 };
+
+
+generate_controlpanel_documentation = function(primary, secondary) {
+	var headline = document.querySelector("span[data-qa-id=\"headerTitle\"]").innerHTML;
+	var xmlhttp=new XMLHttpRequest();
+	    
+	xmlhttp.onreadystatechange=function() {
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+	        console.log(xmlhttp.responseText);
+	  }
+	}
+	    
+	xmlhttp.open("GET",'http://localhost:8080/o/cpdocauthor?primary=' + primary + '&secondary=' + secondary + '&headline=' + headline ,true);
+	xmlhttp.send();
+};
+
